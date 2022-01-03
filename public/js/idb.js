87996bfbc0b4d61bd0c45d5d50561615c9ef0f1f
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-03 13:37:51 -0500

    working on idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,25 @@
 // variable to hold db connection
 let db;
 
-
+// connect to db called budget_tracker
 const request = indexedDB.open("budget_tracker", 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function(event) {
+  // save a reference to the database 
+  const db = event.target.result;
+  // create an object store (table) called `new_budget`
+  db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+// upon successful
+request.onsuccess = function(event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    // uploadBudget();
+  }
+}
+
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+};
