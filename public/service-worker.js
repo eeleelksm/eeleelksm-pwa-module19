aec560f317e805f3b4923c260cc599a763ba73c0
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-04 11:53:55 -0500

    added manifest.json file skeleton

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./js/index.js",
+  "./js/idb.js",
+];
+
+self.addEventListener("install", function (e) {
+  // e.waitUntil(
+  //   caches.open(CACHE_NAME).then(function (cache) {
+  //     console.log("installing cache : " + CACHE_NAME)
+  //     return cache.addAll(FILES_TO_CACHE)
+  //   })
+  // )
+})
